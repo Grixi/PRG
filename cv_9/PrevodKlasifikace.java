0f7574be7322f5a27109a1e227e30d2f353ab1b6
Grixi <adam.tretera@educanet.cz>
2018-09-24 09:13:36 +0200

    hello

--- cv_9/PrevodKlasifikace.java
+++ cv_9/PrevodKlasifikace.java
@@ -1,3 +1,4 @@
+<<<<<<< current
 public class PrevodKlasifikace{
     public static void main (String[] args){
 
@@ -22,3 +23,28 @@ public class PrevodKlasifikace{
 }
     
         
+=======
+public class PrevodKlasifikace{
+    public static void main (String[] args){
+
+        int znamka;
+        znamka = Integer.parseInt(args[0]);
+        if (znamka < 50) {
+            System.out.println("Tvoje znamka je 5.");
+        }
+        if (znamka < 60 && znamka >= 50) {
+            System.out.println("Dostanes 4");
+        }
+        if (znamka < 80 && znamka >= 60) {
+            System.out.println("Mas za 3");
+        }
+        if (znamka < 90 && znamka >= 80) {
+            System.out.println("2 jen tak dal");
+        }
+        if (znamka > 91) {
+            System.out.println("Excelentni práce za |2");
+
+        }
+    }
+}
+>>>>>>> before discard
