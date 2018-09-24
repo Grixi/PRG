d6d4adb71b8636540d635003040f87410db48602
Grixi <adam.tretera@educanet.cz>
2018-09-24 09:48:09 +0200

    trx

--- cv_9/PrevodKlasifikace.java
+++ cv_9/PrevodKlasifikace.java
@@ -1,4 +1,3 @@
-<<<<<<< current
 public class PrevodKlasifikace{
     public static void main (String[] args){
 
@@ -18,33 +17,6 @@ public class PrevodKlasifikace{
         }
         if (znamka > 90) {
             System.out.println("Excelentni práce za 1");
-        }    
-    }
-}
-    
-        
-=======
-public class PrevodKlasifikace{
-    public static void main (String[] args){
-
-        int znamka;
-        znamka = Integer.parseInt(args[0]);
-        if (znamka < 50) {
-            System.out.println("Tvoje znamka je 5.");
-        }
-        if (znamka < 60 && znamka >= 50) {
-            System.out.println("Dostanes 4");
-        }
-        if (znamka < 80 && znamka >= 60) {
-            System.out.println("Mas za 3");
-        }
-        if (znamka < 90 && znamka >= 80) {
-            System.out.println("2 jen tak dal");
-        }
-        if (znamka > 91) {
-            System.out.println("Excelentni práce za |2");
-
         }
     }
 }
->>>>>>> before discard
